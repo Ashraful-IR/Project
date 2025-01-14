abf74e28ceef99da9e2f7919f272a73746b911b7
Ash Raf <rifo0118@gmail.com>
2025-01-14 12:21:11 +0600

    B-colour Changed

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -33,12 +33,13 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.button1.Location = new System.Drawing.Point(325, 408);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(183, 79);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form2
             // 
