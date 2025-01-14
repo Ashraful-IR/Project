d131cd1ad69828eb0be815c27d949b53cbbd2ce0
Ash Raf <rifo0118@gmail.com>
2025-01-14 12:20:04 +0600

    Button Modified

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(124, 98);
+            this.button1.Location = new System.Drawing.Point(325, 408);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(554, 243);
+            this.button1.Size = new System.Drawing.Size(183, 79);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
