ae4d09c58ac0cb532c8fda719111fce195e974b3
Ash Raf <rifo0118@gmail.com>
2025-01-14 13:23:13 +0600

    M

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(594, 284);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(906, 605);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form3";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -44,5 +55,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
